299a9b39d593c42e1322dda1307efe1ea8214df2
Urcodingbuddy <anshpethe110@gmail.com>
2026-04-27 22:14:11 +0530

    feat: integrate Google Tag Manager (GTM-KL3ZD5DD)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Outfit, Great_Vibes } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import ScrollReveal from '@/components/ui/ScrollReveal'
 import FloatingActions from '@/components/ui/FloatingActions'
@@ -100,7 +101,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className={`${outfit.className} ${greatVibes.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KL3ZD5DD');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="antialiased bg-background">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KL3ZD5DD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
